9299e567eab186eaa301c4650be6572935b944b1
rokuta1059 <rokuta1059@gmail.com>
2019-01-11 20:28:04 +0900

    Update W5_P2.java

--- W5_P2.java
+++ W5_P2.java
@@ -11,102 +11,70 @@ public class W5_P2 {
 		
 		return c;
 	}
-	
 
 	public static void main(String[] args) {
-		char[][] foxarr = make2DArray("the quick brown fox jumps over the lazy dog");
+		
+		char[][] foxkey = make2DArray("the quick brown fox jumps over the lazy dog");
 		
 		Scanner input = new Scanner(System.in);
 		
-		while(input.hasNext()) {
-			
-			char[][] unfoxarr = make2DArray(input.nextLine());
-			char[][] aarr = make2DArray(input.nextLine());
-			char[][] barr = make2DArray(input.nextLine());
-			
-			// set map
-			
-			char[] map = new char[255];
-			char[] unmap = new char[255];
-			for (int i = 'a'; i <= 'z'; i++) {
-				map[i] = '?';
-				unmap[i] = '?';
-			}
-			
-			for(int i = 0; i < unfoxarr.length; i++){
-				for(int j = 0; j < unfoxarr[i].length; j++) {
-					if(unfoxarr[i][j] != '?') {
-						unmap[foxarr[i][j]] = unfoxarr[i][j];
-					}
+		char[][] foxvalue = make2DArray(input.nextLine());
+		char[][] keyWord = make2DArray(input.nextLine());
+		char[][] valueWord = make2DArray(input.nextLine());
+		
+		char[] map = new char[255];
+		char[] inv = new char[255];
+		
+		
+		for (int i = 0; i < foxkey.length; i++) {
+			for (int j = 0; j < foxkey[i].length; j++) {
+				if(foxvalue[i][j] != '?') {
+					map[foxkey[i][j]] = foxvalue[i][j];
+					inv[foxvalue[i][j]] = foxkey[i][j];
 				}
 			}
-			
-			for(int i = 0; i < barr.length; i++) {
-				
-				for(int k = 0; k < aarr.length; k++) {
-					boolean possible = false;
-					if(barr[i].length == aarr[k].length) {
-						for(int j = 0; j < barr[i].length; j++) {
-							if(unmap[aarr[k][j]] != '?' && barr[i][j] == unmap[aarr[k][j]]) {
-								possible = true;
-								map[aarr[k][j]] = barr[i][j];
-								continue;
-							} else if (unmap[aarr[k][j]] == '?') {
-								map[aarr[k][j]] = barr[i][j];
-								possible = true;
-								continue;
-							}
-							possible = false;
-							break;
-						}
-					}
-					for(int t = 'a'; t<='z'; t++) {
-						for(int u = t+1; u < 'z'; u++) {
-							if(map[t] != 0 && map[u] != 0 && map[t] != '?' && map[t] != '?' && map[t] == map[u]) {
-								possible = false;
-								break;
-							}
-						}
-					}
-					
-					map = new char[255];
-					
-					if (possible) {
-						
-						if(aarr[k].length <= barr[i].length) {
-							for(int a1 = 0; a1 < aarr[k].length; a1++) {
-								unmap[aarr[k][a1]] = barr[i][a1];
-							}
-						} else {
-							for(int a1 = 0; a1 < barr[i].length; a1++) {
-								unmap[aarr[k][a1]] = barr[i][a1];
-							}
-						}
-						
-					}
-				}
-			} 
-			
-
-			for(int i = 'a'; i <= 'z'; i++) {
-				System.out.print((char)i);
-			}
-			System.out.println();
-			for(int i = 'a'; i <= 'z'; i++) {
-				System.out.print(unmap[i]);
-			}
-			System.out.println();
-			
-			for (int i = 0; i < foxarr.length; i++) {
-				for (int j = 0; j < foxarr[i].length; j++) {
-					System.out.print(unmap[foxarr[i][j]]);
-				}
-				System.out.print(" ");
-			}
-			System.out.println();
 		}
+		
+		boolean impossible = false;
+		
+		for (char[] first : keyWord) {
+			for (char[] second : valueWord) {
+				if (first.length != second.length)
+					continue;
+				for(int i = 0; i < first.length; i++) {
+					if((map[first[i]] != 0 && map[first[i]] != second[i])
+							|| (inv[second[i]] != 0 && inv[second[i]] != first[i])) {
+						for(int j = 0; j < i; j++) {
+							map[first[j]] = 0;
+							inv[second[j]] = 0;
+						}
+						impossible = true;
+						break;
+						
+					} else if (map[first[i]] == 0) {
+						map[first[i]] = second[i];
+						inv[second[i]] = first[i];
+					}
+				}
+				if(!impossible) {
+					break;
+				}
+			}
+			
+		}
+		
+		for (int i = 0; i < foxvalue.length; i++) {
+			for (int j = 0; j < foxkey[i].length; j++) {
+				if(map[foxkey[i][j]] != 0)
+					System.out.print(map[foxkey[i][j]]);
+				else
+					System.out.print("?");
+			}
+			System.out.print(" ");
+		}
+		
+		input.close();
 
 	}
-	
 
 }
