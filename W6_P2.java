07b0f0580e873ea0c2a98e58aceb93025d295f39
rokuta1059 <rokuta1059@gmail.com>
2019-01-11 20:33:27 +0900

    Update W6_P2.java

--- W6_P2.java
+++ W6_P2.java
@@ -1,9 +1,12 @@
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class W6_P2 {
 
-public static void main(String[] args) {
-		
+	public static void main(String[] args) {
+
 		Scanner input = new Scanner(System.in);
 		
 		while(input.hasNext()) {
@@ -19,22 +22,77 @@ public static void main(String[] args) {
 				mush[i] = Integer.parseInt(mushS[i]);
 			}
 			
-			for (int i = 0; i < mush.length; i++) {
-				for (int j = i + 1; j < mush.length; j++) {
-					if(mush[i] == mush[j] || mush[i]+1 == mush[j]) {
-						int temp = mush[i];
-						mush[i] = mush[j];
-						mush[j] = temp;
+			quicksort(mush, 0, n-1);
+			
+			Stack<Integer> finalMush = new Stack<Integer>();
+			LinkedList<Integer> mushroom = new LinkedList<Integer>();
+			for (int i : mush) {
+				mushroom.add(i);
+			}
+			
+			int k = 0;
+			while(finalMush.size() != n) {
+
+				if(finalMush.isEmpty()) {
+					finalMush.push(mushroom.removeFirst());
+				} else {
+					int j = 0;
+					while (j < mushroom.size() - k && finalMush.peek() + 1 == mushroom.get(j)) {
+						j++;
 					} 
+					if (mushroom.size() - k == j) {
+						k++;
+						mushroom.addLast(finalMush.pop());
+					} else {
+						finalMush.push(mushroom.get(j));
+						mushroom.remove(j);
+						k = 0;
+						Collections.sort(mushroom);
+					} 
+					
 				}
 			}
 			
-			for(int i = 0; i < mush.length; i++) {
-				System.out.print(mush[i] + " ");
+			Collections.reverse(finalMush);
+			
+			while(!finalMush.empty()) {
+				System.out.print(finalMush.pop() + " ");
 			}
 			System.out.println();
 		}
-		
+		input.close();
 		
 	}
+
+	public static void quicksort(int s[], int l, int h) {
+		int p;
+		if (l < h) {
+			p = partition(s, l, h);
+			quicksort(s, l, p-1);
+			quicksort(s, p+1, h);
+		}
+	}
+	
+	public static int partition(int s[], int l, int h) {
+		int i;
+		int p;
+		int firsthigh;
+		
+		p = h;
+		firsthigh = l;
+		for (i = l; i < h; i++) {
+			if (s[i] < s[p]) {
+				int temp = s[i];
+				s[i] = s[firsthigh];
+				s[firsthigh] = temp;
+				firsthigh++;
+			}
+			
+		}
+		int temp = s[p];
+		s[p] = s[firsthigh];
+		s[firsthigh] = temp;
+		return firsthigh;
+	}
+
 }
