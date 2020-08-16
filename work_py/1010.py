9abd042a83ce9a961f855f2308e38433ad32b377
Kim woo seob <rokuta1059@gmail.com>
2020-08-16 16:28:06 +0900

    1010 - 다리 놓기
    내용 수정

--- work_py/1010.py
+++ work_py/1010.py
@@ -4,9 +4,9 @@ for _ in range(t):
     N, M = map(int, stdin.readline().split(' '))
     ans = 1
     if N != M:
-        for i in range(M-N+1, M):
+        for i in range(M, M-N, -1):
             ans *= i
         for i in range(1, N+1):
             ans //= i
-
+            
     print(ans)
